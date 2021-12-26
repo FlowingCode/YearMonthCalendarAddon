9503fc844bd088cfdb9da3895df23060c9cc379d
Javier Godoy <11554739+javier-godoy@users.noreply.github.com>
2021-12-26 14:47:56 -0300

    fix: set event debounce

--- src/main/java/com/flowingcode/addons/ycalendar/DateSelectedEvent.java
+++ src/main/java/com/flowingcode/addons/ycalendar/DateSelectedEvent.java
@@ -2,12 +2,15 @@ package com.flowingcode.addons.ycalendar;
 
 import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.ComponentEvent;
+import com.vaadin.flow.component.DebounceSettings;
 import com.vaadin.flow.component.DomEvent;
 import com.vaadin.flow.component.EventData;
+import com.vaadin.flow.dom.DebouncePhase;
 import java.time.LocalDate;
 import java.util.Objects;
 
-@DomEvent("date-selected")
+@DomEvent(value = "date-selected",
+    debounce = @DebounceSettings(timeout = 200, phases = DebouncePhase.TRAILING))
 public class DateSelectedEvent<T extends Component> extends ComponentEvent<T> {
 
   private static final long serialVersionUID = 1L;
