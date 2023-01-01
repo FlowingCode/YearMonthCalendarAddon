d3cce59fc6b65a70af25c04b2a94d4eaa83ef728
Pranjal <pranjal9.joshi@gmail.com>
2023-01-01 21:34:30 +0530

    Added Support for setWeekNumbersVisible. Fixes [InlineDatePicker] Add support for setWeekNumbersVisible #34
    Added Support for setWeekNumbersVisible in InlineDatePicker.java

--- src/main/java/com/flowingcode/addons/ycalendar/InlineDatePicker.java
+++ src/main/java/com/flowingcode/addons/ycalendar/InlineDatePicker.java
@@ -7,9 +7,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -19,12 +19,14 @@
  */
 package com.flowingcode.addons.ycalendar;
 
+import com.sun.org.apache.xpath.internal.operations.Bool;
 import com.vaadin.flow.component.AbstractSinglePropertyField;
 import com.vaadin.flow.component.HasSize;
 import com.vaadin.flow.component.HasTheme;
 import com.vaadin.flow.component.Tag;
 import com.vaadin.flow.component.dependency.JsModule;
 import com.vaadin.flow.function.SerializableFunction;
+
 import java.time.LocalDate;
 import java.util.Optional;
 
@@ -33,15 +35,22 @@ import java.util.Optional;
 @JsModule("./fc-inline-date-picker/fc-inline-date-picker.js")
 public class InlineDatePicker extends AbstractSinglePropertyField<InlineDatePicker, LocalDate> implements HasSize, HasTheme {
 
-  private static final String VALUE_PROPERTY = "value";
-  
-  private static <R,S> SerializableFunction<R,S> map(SerializableFunction<R,S> f) {
-    return r->Optional.ofNullable(r).map(f).orElse(null);
-  }
-  
-  public InlineDatePicker() {
-    super(VALUE_PROPERTY, null, String.class, map(LocalDate::parse), map(LocalDate::toString));
-    setValue(LocalDate.now());
-  }
+    private static final String VALUE_PROPERTY = "value";
+    private static boolean weekNumbersVisible = false;
+
+    private static <R, S> SerializableFunction<R, S> map(SerializableFunction<R, S> f) {
+        return r -> Optional.ofNullable(r).map(f).orElse(null);
+    }
+
+    public InlineDatePicker() {
+        super(VALUE_PROPERTY, null, String.class, map(LocalDate::parse), map(LocalDate::toString));
+        setValue(LocalDate.now());
+    }
+
+    public void setWeekNumbersVisible(Boolean showWeekNumbers) {
+        if (showWeekNumbers) {
+            weekNumbersVisible = true;
+        }
+    }
 
 }
